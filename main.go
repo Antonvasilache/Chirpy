a1de046b0c803b3607ffdd827a4a31c4bf8b171c
antonvasilache <vasilacheanton91@gmail.com>
2024-11-12 14:41:48 +0200

    server

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main(){
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Handler: mux,
+		Addr: ":8080",
+	}
+
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))	
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
